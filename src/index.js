d881dbc2ca175a26772a11e214783bcade79ee61
xonin-hush <xonindev@gmail.com>
2024-02-16 08:32:59 +0000

    LoadHome on start of the page

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import { loadMenu } from './menu.js';
 import { loadHome } from './home.js'
 import { loadAbout } from './about.js';
 const homeButton = document.querySelector("#button-home")
-console.log(homeButton)
 const menuButton = document.querySelector("#button-menu")
-console.log(menuButton)
 const aboutButton = document.querySelector("#button-about")
-console.log(aboutButton)
-
-console.log(content)
+loadHome()
 homeButton.addEventListener('click', () => {
 
     loadHome()
